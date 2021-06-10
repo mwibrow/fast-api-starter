ccec7efa34fb510192e2bcdb1ca43a14665bd53b
Mark Wibrow <m.wibrow@gmail.com>
2021-06-10 14:28:48 +0100

    Don't subclass FastAPI

--- app/app.py
+++ app/app.py
@@ -1,31 +1,41 @@
 """
 Configure app
 """
+from typing import Awaitable, Callable
 from fastapi import FastAPI
 from .config import APP_NAME, APP_VERSION
 
 
-class Application(FastAPI):
-    """Application class."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.add_event_handler("startup", self.startup)
-        self.add_event_handler("shutdown", self.shutdown)
-
-    async def startup(self):
-        """Start up."""
-
-    async def shutdown(self):
-        """Shutdown."""
-
-
 def get_app() -> FastAPI:
     """Configure application."""
-    app = Application()
+    app = FastAPI()
+    app.add_event_handler("startup", on_startup(app))
+    app.add_event_handler("shutdown", on_shutdown())
 
-    @app.get("/status")
-    async def status():
-        return {"app": APP_NAME, "version": APP_VERSION, "message": "healthy"}
+    app.get("/status")(status)
 
     return app
+
+
+def on_startup(app: FastAPI) -> Callable[[], Awaitable[None]]:
+    """Return a handler executed when the `startup` event is triggered."""
+
+    async def startup():
+        """Do something on start up."""
+        app.state.some_attribte = None
+
+    return startup
+
+
+def on_shutdown() -> Callable[[], Awaitable[None]]:
+    """Return a handler executed when the `shutdown` event is triggered."""
+
+    async def shutdown():
+        """Do something on shutdown."""
+
+    return shutdown
+
+
+async def status():
+    """Status handler."""
+    return {"app": APP_NAME, "version": APP_VERSION, "message": "healthy"}
